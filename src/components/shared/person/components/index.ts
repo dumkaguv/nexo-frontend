12418405c42352326e004787ea85d601952477fb
DmitriiPC <68027431+dumkaguv@users.noreply.github.com>
2025-10-04 17:07:12 +0300

    Normalize casing

--- src/components/shared/person/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { PersonName as Name } from './PersonName'
-export { PersonNickname as Nickname } from './PersonNickname'
-export { PersonFollowInfo as FollowInfo } from './PersonFollowInfo'
-export { PersonAvatar as Avatar } from './PersonAvatar'
