1101c49d9a041b66b8e68117a9aafed780e91711
Dmitrii Golovicichin <dmitrii.golovicichin@gmail.com>
2026-01-17 10:33:45 +0200

    chore: update WS types from back-end

--- src/stores/WebSocketContext.ts
+++ src/stores/WebSocketContext.ts
@@ -1,10 +1,18 @@
 import { createContext } from 'react'
 
 import type {
-  CreateMessageDto,
-  ResponseConversationDto,
-  ResponseMessageDto,
-  UpdateMessageDto
+  PostWsConversationsEventConversationDeletedResponse,
+  PostWsConversationsEventConversationNewResponse,
+  PostWsMessagesEventMessageDeletedResponse,
+  PostWsMessagesEventMessageNewResponse,
+  PostWsMessagesEventMessageSentResponse,
+  PostWsMessagesEventMessageUpdatedResponse,
+  PostWsMessagesMessageDeleteData,
+  PostWsMessagesMessageSendData,
+  PostWsMessagesMessageUpdateData,
+  PostWsUsersEventUserOfflineResponse,
+  PostWsUsersEventUserOnlineListResponse,
+  PostWsUsersEventUserOnlineResponse
 } from '@/api'
 import type { CLIENT_TO_SERVER_EVENTS, SERVER_TO_CLIENT_EVENTS } from '@/config'
 
@@ -15,30 +23,48 @@ import type { Socket } from 'socket.io-client'
 type ServerToClient = {
   [SERVER_TO_CLIENT_EVENTS.exception]: (error: WsError) => void
 
-  [SERVER_TO_CLIENT_EVENTS.user.online]: (userId: number) => void
-  [SERVER_TO_CLIENT_EVENTS.user.offline]: (userId: number) => void
-  [SERVER_TO_CLIENT_EVENTS.user.onlineList]: (userIds: number[]) => void
+  [SERVER_TO_CLIENT_EVENTS.user.online]: (
+    payload: PostWsUsersEventUserOnlineResponse
+  ) => void
+  [SERVER_TO_CLIENT_EVENTS.user.offline]: (
+    payload: PostWsUsersEventUserOfflineResponse
+  ) => void
+  [SERVER_TO_CLIENT_EVENTS.user.onlineList]: (
+    payload: PostWsUsersEventUserOnlineListResponse
+  ) => void
 
   [SERVER_TO_CLIENT_EVENTS.conversation.new]: (
-    dto?: ResponseConversationDto
+    payload: PostWsConversationsEventConversationNewResponse
   ) => void
-  [SERVER_TO_CLIENT_EVENTS.conversation.deleted]: (dto?: {
-    deletedConversationId?: number
-  }) => void
+  [SERVER_TO_CLIENT_EVENTS.conversation.deleted]: (
+    payload: PostWsConversationsEventConversationDeletedResponse
+  ) => void
 
-  [SERVER_TO_CLIENT_EVENTS.message.new]: (dto?: ResponseMessageDto) => void
-  [SERVER_TO_CLIENT_EVENTS.message.sent]: (dto?: ResponseMessageDto) => void
-  [SERVER_TO_CLIENT_EVENTS.message.updated]: (dto?: ResponseMessageDto) => void
-  [SERVER_TO_CLIENT_EVENTS.message.deleted]: (dto?: {
-    deletedMessageId: number
-  }) => void
+  [SERVER_TO_CLIENT_EVENTS.message.new]: (
+    payload: PostWsMessagesEventMessageNewResponse
+  ) => void
+  [SERVER_TO_CLIENT_EVENTS.message.sent]: (
+    payload: PostWsMessagesEventMessageSentResponse
+  ) => void
+  [SERVER_TO_CLIENT_EVENTS.message.updated]: (
+    payload: PostWsMessagesEventMessageUpdatedResponse
+  ) => void
+  [SERVER_TO_CLIENT_EVENTS.message.deleted]: (
+    payload: PostWsMessagesEventMessageDeletedResponse
+  ) => void
 }
 
 type ClientToServer = {
   [CLIENT_TO_SERVER_EVENTS.user.onlineListRequest]: () => void
-  [CLIENT_TO_SERVER_EVENTS.message.send]: (dto: CreateMessageDto) => void
-  [CLIENT_TO_SERVER_EVENTS.message.update]: (dto: UpdateMessageDto) => void
-  [CLIENT_TO_SERVER_EVENTS.message.delete]: (dto: { id: number }) => void
+  [CLIENT_TO_SERVER_EVENTS.message.send]: (
+    dto: PostWsMessagesMessageSendData['body']
+  ) => void
+  [CLIENT_TO_SERVER_EVENTS.message.update]: (
+    dto: PostWsMessagesMessageUpdateData['body']
+  ) => void
+  [CLIENT_TO_SERVER_EVENTS.message.delete]: (
+    dto: PostWsMessagesMessageDeleteData['body']
+  ) => void
 }
 
 export type WebSocketNamespaceValue = {
